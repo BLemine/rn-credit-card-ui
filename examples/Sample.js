a9e74cfac0a1f6581af5993add4f228d265ad493
BLemine <baillahiamine@gmail.com>
2021-07-04 14:42:12 +0100

    added example + doc-refactoring

---
+++ examples/Sample.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CreditCardUI from "../App"
+export default () => <CreditCardUI
+    creditCardNumber="4242424242424242"
+    cardCVC="123"
+    cardHolderGender="miss"
+    cardHolderName="Maria James"
+    cardExpiryDate="11/23"
+    /**
+     *  Not required
+     */
+    // frontImg={{ uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Earth_Eastern_Hemisphere.jpg/1024px-Earth_Eastern_Hemisphere.jpg" }} // or require("")
+    // or 
+    //frontImg={require("./assets/images/earth.jpg")}
+    // backImg={{ uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Earth_Eastern_Hemisphere.jpg/1024px-Earth_Eastern_Hemisphere.jpg" }}
+    // textColor="yellow"
+    // secureCardNbr={true}
+/>
